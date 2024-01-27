95f8955e7afa4b03f180630e8d651722f58fd9af
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2024-01-26 23:44:25 -0500

    Scrape pronto and cartelera

--- src/scrape_cineco/_utils.py
+++ src/scrape_cineco/_utils.py
@@ -1,18 +1,31 @@
-SPANISH_MONTH_TO_NUMBER_MAP = {
-    "ene": "01",
-    "feb": "02",
-    "mar": "03",
-    "abr": "04",
-    "may": "05",
-    "jun": "06",
-    "jul": "07",
-    "ago": "08",
-    "sep": "09",
-    "oct": "10",
-    "nov": "11",
-    "dic": "12",
-}
+import httpx
+from bs4 import BeautifulSoup
+
+from scrape_cineco import settings
 
 
 def spanish_month_to_number(month: str) -> str:
-    return SPANISH_MONTH_TO_NUMBER_MAP[month]
+    return {
+        "ene": "01",
+        "feb": "02",
+        "mar": "03",
+        "abr": "04",
+        "may": "05",
+        "jun": "06",
+        "jul": "07",
+        "ago": "08",
+        "sep": "09",
+        "oct": "10",
+        "nov": "11",
+        "dic": "12",
+    }[month]
+
+
+def get_soup(url: str) -> str:
+    r = httpx.get(url)
+    return BeautifulSoup(r.text, "lxml")
+
+
+def save_html(url: str, filename: str) -> None:
+    with open(f"{settings.local_tmp_dir}/{filename}.html", "w") as f:
+        f.write(get_soup(url).prettify())
